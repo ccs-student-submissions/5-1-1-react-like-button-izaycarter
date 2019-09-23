e58e3536c35f6d7114c8ee1852c6bd3bcddc18ea
Isaiah Carter <izay.carter0509@gmail.com>
2019-09-23 16:53:05 -0400

    button is finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Likes/>
+      </div>
+    );
+  }
+}
+
+
+class Likes extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {count:0};
+    }
+
+    handleClick = () => {
+        this.setState(({count}) => ({
+            count: count + 1
+        }));
+
+    }
+
+  render() {
+    return <button className="button" onClick={this.handleClick}>{this.state.count} {this.state.count === 1 ? "like":"likes"}</button>;
+  }
 }
 
 export default App;
